5609840621d876e5fffb8ff4dbb7a7e35efe10e6
AF3 <af@af3.tech>
2020-09-07 02:05:36 -0400

    TCP chatroom implementation

--- client.py
+++ client.py
@@ -8,53 +8,65 @@ import threading
 
 #class to listen for keyboard input
 class keyboardThread ( threading.Thread ):
-    def __init__( self, ): # define the constructor with appropriate parameters
+    def __init__( self, sock): # define the constructor with appropriate parameters
         threading.Thread.__init__( self )
         # initilize instance fields
-        
+        self.sock = sock
     def run( self ):
         while True:
             try:
                 # get message from user using input
-                
+                # message = sys.stdin.readline()
+                message = input()
                 # if first character is ".", add a second "." to the beginning
-               
+                if message[0] == ".":
+                    message = "." + message
                 # send the message to the server
-
+                self.sock.sendall(message.encode())
             except:
                 # ^d entered, so exit loop
                 break
  
         # send a message consisting of "." to server
-        
+        self.sock.sendall(".".encode())
  #class to listen for chat messages from the server       
 class chatThread ( threading.Thread ):
-    def __init__( self ): #define the constructor with appropriate parameters
+    def __init__( self, sock ): #define the constructor with appropriate parameters
         threading.Thread.__init__( self )
         # initialize instance fields
-        
+        self.sock = sock
     def run( self ):
         while True:
             # get message from server
-            
+            message = self.sock.recv(1024).decode()
             # if the message has at least one byte of data, print it out
-            
-            # else break    
+            if message:
+                print(message)
+            # else break 
+            else:
+                break   
 
 #main
 # Create a TCP/IP socket
-
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # get command line arguments
-
+if len(sys.argv) == 4:
+    server = sys.argv[1]
+    port = int(sys.argv[2])
+    userHandle = sys.argv[3]
+else:
+    print ("usage: python3 client.py <server> <port> <userHandle>")
+    sys.exit()
 # Connect the socket to the port where the server is listening
-
+server_address = (server, port)
+sock.connect(server_address)
 
 # send the user's handle to the server
-
+sock.sendall(userHandle.encode())
 #start threads to handle the communication
-kb = keyboardThread( ) #pass parameters defined in the constructor
+kb = keyboardThread(sock) #pass parameters defined in the constructor
 kb.start()
-chat = chatThread ( ) # pass parameters defined in the constructor
+chat = chatThread (sock) # pass parameters defined in the constructor
 chat.start ()
 
 #wait for the threads to end, which indicates that the user has left the room
@@ -62,6 +74,6 @@ kb.join()
 chat.join()
 
 # close the TCP connection
-
+sock.close()
 
 
--- server.py
+++ server.py
@@ -8,50 +8,76 @@ import threading
 
 #class to service one client
 class serverThread ( threading.Thread ):
-    def __init__( self ): # define a constructor with appropriate parameters
+    def __init__( self, connection, client_address, handle ): # define a constructor with appropriate parameters
         threading.Thread.__init__( self )
         # initialize other appropriate instance fields
-               
+        self.connection = connection
+        self.client_address = client_address
+        self.handle = handle
+
     def run( self ):
         # send message to everyone letting them know that we're in the room
-        
+        handleWelcome = self.handle + " has joined the chatroom!"
+        for host in connections:
+            if host != self.connection:
+                host.send(handleWelcome.encode())
         while True:
             # read a message from the client
-            
+            message = self.connection.recv(1024).decode()
             # if the message is ".":
                 # close the connection
                 # break
+            if message == ".":
+                break
             # if the first character is "."
                 # remove the first character
+            if message[0] == ".":
+                message = message[1:]
             # add the user's handle to the front of the message
             # send this message to all other clients
-        
+            if message:
+                handleMessage = self.handle + ": " + message
+                for host in connections:
+                    if host != self.connection:
+                        host.send(handleMessage.encode())
         # send message to all other clients letting them know this user is leaving the chat room
-            
+        handleGoodbye = self.handle + " has left the chatroom!"
+        for host in connections:
+            if host != self.connection:
+                host.send(handleGoodbye.encode())
         # remove this client from any data structures
-        
+        connections.remove(self.connection)
+        print(self.client_address, " disconnected")
         # close the TCP connection
-        
+        self.connection.close()
 
 # main
 # Create a TCP/IP socket
-
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # get command line arguments
-
+if len(sys.argv) == 2:
+    port = int(sys.argv[1])
+else:
+    print ("usage: python3 server.py <port>")
+    sys.exit()
 # Bind the socket to the port
-
+server_address = ('localhost', port)
+print ('starting up on {} port {}'.format(server_address[0], server_address[1]))
+sock.bind(server_address)
 # Listen for incoming connections
-
+sock.listen()
 #initialize any data structures
-
+connections = []
 while True:
     # Wait for a connection
-    
+    print ('waiting for a connection')
+    connection, client_address = sock.accept()
+    print(client_address, " has connected")
     # read the first message from the client, which will be the user's handle
-    
+    handle = connection.recv(1024).decode()
     # add this client to any data structures
-    
+    connections.append(connection)
     # create a thread to handle this client and start that thread
-    thread = serverThread(  ) # You'll have to pass whatever parameters you defined in your constructor
+    thread = serverThread(connection, client_address, handle) # You'll have to pass whatever parameters you defined in your constructor
     thread.start()
 
